ac355bcabb6008e8241846b34a35e21cd6ddc528
sjrv <sjorvv@gmail.com>
2015-01-09 21:20:15 +0100

    Remove unecessary migrations

--- db/migrate/20150106133936_create_likes.rb
+++
@@ -1,4 +0,0 @@
-class CreateLikes < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20150106135643_create_likes_list.rb
+++
@@ -1,8 +0,0 @@
-class CreateLikesList < ActiveRecord::Migration
-  def change
-    create_table :likes do |t|
-      t.integer :user_id
-      t.integer :answer_id
-    end
-  end
-end
